bc42da18aa31c2d64b7e4225114e2232bc089e41
samcien <85210751+samcien@users.noreply.github.com>
2024-12-11 10:42:22 -0500

    Delete licitaciones directory

--- licitaciones/app/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:LbvDgiSovKycsLRnCylcpsruUzptMfeo@junction.proxy.rlwy.net:29230/railway'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
